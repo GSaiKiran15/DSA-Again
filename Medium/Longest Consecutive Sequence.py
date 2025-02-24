b36fbb486900e15d80a3ba85b41710bb5975e884
Sai Kiran Ganesh Kumar <ganeshsaikiran7@gmail.com>
2025-02-24 15:00:23 -0700

    24-02-2025

---
+++ Medium/Longest Consecutive Sequence.py
@@ -0,0 +1,12 @@
+nums = [0,0]
+num_set = set(nums)
+longest = 0
+for n in nums:
+    if (n-1) not in num_set:
+        length = 0
+        while (n+length) in num_set:
+            length += 1
+        longest = max(length, longest)
+print(longest)
+
+       
