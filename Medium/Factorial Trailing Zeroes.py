a15ba66d63503baab76548fa6c61a757d65bed50
Sai Kiran Ganesh Kumar <ganeshsaikiran7@gmail.com>
2025-03-30 20:10:02 -0600

    30-03-2025

---
+++ Medium/Factorial Trailing Zeroes.py
@@ -0,0 +1,6 @@
+n = 5
+ans = 0
+while n>0:
+    n //= 5
+    ans += n
+print(ans)
--- Medium/Spiral Matrix.py
+++ Medium/Spiral Matrix.py
@@ -1,64 +1,39 @@
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
+def spiralOrder(matrix):
+    if not matrix:
+        return []
 
-m = len(matrix)
-n = len(matrix[0])
-res = []
-visited = set()
-left = 0
+    res = []
+    left, right = 0, len(matrix[0])
+    top, bottom = 0, len(matrix)
 
-def move_right(i,j):
-    print("move right")
-    next_j = 0
-    for move in range(j,n):
-        if (i,move) in visited:
-            move_down(i+1,j)
-        else:
-            res.append(matrix[i][move])
-            visited.add((i,move))
-        next_j = move
-    left += 1
-    if len(res) == m*n:
-        return res
-    else:
-        move_down(i+1, next_j)
+    while left < right and top < bottom:
+        # Move right
+        for i in range(left, right):
+            res.append(matrix[top][i])
+        top += 1
 
-def move_down(i,j):
-    next_i = 0
-    print("move down")
-    for move in range(i,m):
-        if (move,j) in visited:
-            move_left(i,j-1)
-        else:
-            res.append(matrix[move][j])
-            visited.add((move,j))
-        next_i = move
-    if len(res) == m*n:
-        return res
-    else:
-        move_right(next_i, j)
+        # Move down
+        for i in range(top, bottom):
+            res.append(matrix[i][right - 1])
+        right -= 1
 
-def move_left(i,j):
-    print("move left")
-    for move in range(j-1,left-1, -1):
-        if (i,move) in visited:
-            move_right(i+1,j)
-        else:
-            res.append(matrix[i][move])
-            visited.add((i,move))
-    if len(res) == m*n:
-        return res
-    else:
-        move_up()
+        if not (left < right and top < bottom):
+            break
 
-def move_up(i,j):
-    print("move up")
-    for move in range():
-        if (i,move) in visited:
-            move_down(i+1,j)
-        else:
-            res.append(matrix[i][move])
-            visited.add((i,move))
-    if len(res) == m*n:
-        return res
+        # Move left
+        for i in range(right - 1, left - 1, -1):
+            res.append(matrix[bottom - 1][i])
+        bottom -= 1
 
-move_right(0,0)
+        # Move up
+        for i in range(bottom - 1, top - 1, -1):
+            res.append(matrix[i][left])
+        left += 1
+
+    return res
+
+# Define the matrix
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# Call the function and print the result
+print(spiralOrder(matrix))
