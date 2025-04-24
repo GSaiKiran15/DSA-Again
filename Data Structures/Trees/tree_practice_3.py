b60dbaf80abd546c8afc447d5f627ea86fa5798c
Sai Kiran Ganesh Kumar <ganeshsaikiran7@gmail.com>
2025-04-24 17:21:31 -0600

    tree practice

--- Data Structures/Trees/tree_practice_3.py
+++ Data Structures/Trees/tree_practice_3.py
@@ -8,10 +8,36 @@ class TreeNode:
         self.children.append(child)
         child.parent = self
         
-    def print_tree(self):
-        print(self.data)
-        if self.children:
-            
+    def print_tree(self, level=0):
+        print("__" * level + self.value)
+        if self.children: 
+            level += 1           
             for i in self.children:
-                self.print_tree(i)
-        
+                i.print_tree(level)
+    
+def build_tree():
+    root = TreeNode("Electronics")
+
+    laptop = TreeNode("Laptop")
+    mobile = TreeNode("Mobile")
+    camera = TreeNode("Camera")
+
+    laptop.add_child(TreeNode("ASUS"))
+    laptop.add_child(TreeNode("ACER"))
+    laptop.add_child(TreeNode("Lenovo"))
+
+    mobile.add_child(TreeNode("Samsung"))
+    mobile.add_child(TreeNode("Apple"))
+    mobile.add_child(TreeNode("Mi"))
+
+    camera.add_child(TreeNode("Canon"))
+    camera.add_child(TreeNode("Nikon"))
+    camera.add_child(TreeNode("RED"))
+
+    root.add_child(laptop)
+    root.add_child(mobile)
+    root.add_child(camera)
+
+    root.print_tree()
+
+build_tree()
